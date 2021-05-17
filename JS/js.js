fbe6e157a6b4695ec1e533fc79e3b22d3101208c
VictorNorm <VictorNorm@users.noreply.github.com>
2021-05-17 11:06:47 +0200

    Updated carousel.

--- JS/js.js
+++ JS/js.js
@@ -4,6 +4,7 @@ const url = "https://hjulbent.no/project-exam/wp-json/wp/v2/posts?per_page=12";
 // ?per_page=100
 
 const carousel = document.querySelector(".carousel");
+const slider = document.querySelector(".slider");
 
 const carouselContainer = document.querySelector(".carouselContainer");
 
@@ -27,38 +28,32 @@ async function getPosts() {
             if (i < 4) {
                 cardContainer1.innerHTML +=
                     `
-                <a href="specific.html?id=${results[i].id}" class="displayed postLink">
-                    <div class="carouselCard" style="background-image: url('${results[i].featured_media_src_url}')">
+                    <a href="specific.html?id=${results[i].id}" class="displayed postLink" style="background-image: url('${results[i].featured_media_src_url}')">
                         <h5 class="carouselCardHeading">${results[i].title.rendered}</h5>
-                    </div>
-                 </a>  
-                `;
+                    </a>  
+                    `;
             } else if (i < 8) {
                 cardContainer2.innerHTML +=
-                `
-                <a href="specific.html?id=${results[i].id}" class="displayed postLink">
-                    <div class="carouselCard" style="background-image: url('${results[i].featured_media_src_url}')">
+                    `
+                    <a href="specific.html?id=${results[i].id}" class="displayed postLink" style="background-image: url('${results[i].featured_media_src_url}')">
                         <h5 class="carouselCardHeading">${results[i].title.rendered}</h5>
-                    </div>
-                 </a>  
-                `;
-        } else {
-            cardContainer3.innerHTML +=
-                `
-                <a href="specific.html?id=${results[i].id}" class="displayed postLink">
-                    <div class="carouselCard" style="background-image: url('${results[i].featured_media_src_url}')">
+                    </a>  
+                    `;
+            } else {
+                cardContainer3.innerHTML +=
+                    `
+                    <a href="specific.html?id=${results[i].id}" class="displayed postLink" style="background-image: url('${results[i].featured_media_src_url}')">
                         <h5 class="carouselCardHeading">${results[i].title.rendered}</h5>
-                    </div>
-                 </a>  
-                `;
+                    </a>  
+                    `;
+            }
+
         }
-    
+
+
+    } catch (error) {
+        console.log(error);
     }
-
-
-} catch (error) {
-    console.log(error);
-}
 }
 
 
@@ -70,22 +65,22 @@ console.log(carouselContainer.scrollWidth);
 
 // Carousel----------------------------------------------/
 
-var carouselPosition = carousel.style.transform;
+// var carouselPosition = carousel.style.transform;
 
 buttonNext.addEventListener('click', () => {
-    if(carousel.style.transform === "") {
-        carousel.style.transform = "translateX(-33.3%)";
-    } else if(carousel.style.transform === "translateX(-33.3%)") {
-        carousel.style.transform = "translateX(-66.6%)";
+    if (slider.style.transform === "") {
+        slider.style.transform = "translateX(-33.3%)";
+    } else if (slider.style.transform === "translateX(-33.3%)") {
+        slider.style.transform = "translateX(-66.6%)";
     }
 })
 
 
 
 buttonPrev.addEventListener('click', () => {
-    if(carousel.style.transform === "translateX(-66.6%)") {
-        carousel.style.transform = "translateX(-33.3%)";
-    } else if(carousel.style.transform === "translateX(-33.3%)") {
-        carousel.style.transform = "";
+    if (slider.style.transform === "translateX(-66.6%)") {
+        slider.style.transform = "translateX(-33.3%)";
+    } else if (slider.style.transform === "translateX(-33.3%)") {
+        slider.style.transform = "";
     }
 })
