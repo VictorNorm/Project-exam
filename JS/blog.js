c58a8a895011070794fc8dcb419ffcf366fd758f
VictorNorm <VictorNorm@users.noreply.github.com>
2021-05-22 15:41:30 +0200

    updated

--- JS/blog.js
+++ JS/blog.js
@@ -12,9 +12,7 @@ async function getPosts() {
         const response = await fetch(url);
         const results = await response.json();
         console.log(results)
-        // console.log(results[i]._embedded.author[0].name)
         
-
         spinner.style.display = "none";
 
         for (let i = 0; i < results.length; i++) {
@@ -34,10 +32,27 @@ async function getPosts() {
             let shortText = stringCutter(cleanText, length);
             
             
-            if (i < 12) {
+            if (i < 4) {
                 blogsContainer.innerHTML +=
                     `
                     <a href="specific.html?id=${results[i].id}" class="blogCard">
+                        <div class="blogImageWrapper">
+                            <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
+                        </div>
+                        <div class="blogInfo">
+                            <h3 class="blogInfoHeading">${results[i].title.rendered}</h3>
+                             <p>${shortText}...</p>
+                             <div class="authorAndTime">
+                                <p class="author">${results[i]._embedded.author[0].name}.</p>
+                                <p class="time">${results[i].modified.slice(0, 10)}.</p>
+                             </div>
+                        </div>
+                    </a>  
+                    `;
+            } else {
+                blogsContainer.innerHTML +=
+                    `
+                    <a href="specific.html?id=${results[i].id}" class="blogCard hidden">
                         <div class="blogImageWrapper">
                             <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
                         </div>
@@ -62,6 +77,9 @@ async function getPosts() {
 getPosts();
 
 
+
+// Cutting out text for blog cards ------------------------------------------//
+
 function stringCutter(string, length) {
     var test = string.slice(0, length)
     let counter = 1;
@@ -81,3 +99,31 @@ function stripHtml(html){
     // Retrieve the text property of the element (cross-browser support)
     return temporalDivElement.textContent || temporalDivElement.innerText || "";
 }
+
+
+// Show more ----------------------------------------------------------------//
+
+
+const showMore = document.querySelector(".showMore");
+
+const blogCards = document.getElementsByClassName("blogCard");
+
+
+let counter = 4;
+
+showMore.addEventListener('click', (event) => {
+    
+    counter = counter +4;
+    console.log(counter);
+    for(let i = 0; i < blogCards.length; i++){
+        if(blogCards[i].classList[1] === "hidden" && i < counter) {
+            blogCards[i].classList.remove("hidden");
+        } if (counter > blogCards.length) {
+            console.log(counter);
+        }
+    }
+});
+
+console.log(counter);
+
+// showMore.disabled = true;
