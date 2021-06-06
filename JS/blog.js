a433c613a18362a403b8e4b0e0301353cada4494
VictorNorm <VictorNorm@users.noreply.github.com>
2021-06-06 16:42:51 +0200

    updated js blog

--- JS/blog.js
+++ JS/blog.js
@@ -1,4 +1,4 @@
-const url = "https://hjulbent.no/project-exam/wp-json/wp/v2/posts?per_page=12&_embed";
+const url = "https://hjulbent.no/project-exam/wp-json/wp/v2/posts?per_page=4&_embed";
 
 const spinner = document.querySelector(".spinner");
 const blogsContainer = document.querySelector(".blogsContainer");
@@ -9,7 +9,7 @@ async function getPosts() {
 
         const response = await fetch(url);
         const results = await response.json();
-        // console.log(results)
+        console.log(results)
 
         spinner.style.display = "none";
 
@@ -27,9 +27,9 @@ async function getPosts() {
             */
             let shortText = stringCutter(cleanText, length);
 
-            if (i < 4) {
-                blogsContainer.innerHTML +=
-                    `
+            // if (i < 4) {
+            blogsContainer.innerHTML +=
+                `
                     <a href="specific.html?id=${results[i].id}" class="blogCard">
                         <div class="blogImageWrapper">
                             <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
@@ -44,24 +44,27 @@ async function getPosts() {
                         </div>
                     </a>  
                     `;
-            } else {
-                blogsContainer.innerHTML +=
-                    `
-                    <a href="specific.html?id=${results[i].id}" class="blogCard hidden">
-                        <div class="blogImageWrapper">
-                            <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
-                        </div>
-                        <div class="blogInfo">
-                            <h3 class="blogInfoHeading">${results[i].title.rendered}</h3>
-                             <p class="shortText">${shortText}...</p>
-                             <div class="authorAndTime">
-                                <p class="author">${results[i]._embedded.author[0].name}.</p>
-                                <p class="time">${results[i].modified.slice(0, 10)}.</p>
-                             </div>
-                        </div>
-                    </a>  
-                    `;
-            }
+            //  } 
+
+
+            // else {
+            //     blogsContainer.innerHTML +=
+            //         `
+            //         <a href="specific.html?id=${results[i].id}" class="blogCard hidden">
+            //             <div class="blogImageWrapper">
+            //                 <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
+            //             </div>
+            //             <div class="blogInfo">
+            //                 <h3 class="blogInfoHeading">${results[i].title.rendered}</h3>
+            //                  <p class="shortText">${shortText}...</p>
+            //                  <div class="authorAndTime">
+            //                     <p class="author">${results[i]._embedded.author[0].name}.</p>
+            //                     <p class="time">${results[i].modified.slice(0, 10)}.</p>
+            //                  </div>
+            //             </div>
+            //         </a>  
+            //         `;
+            // }
         }
 
     } catch (error) {
@@ -70,6 +73,10 @@ async function getPosts() {
 }
 
 getPosts();
+
+
+
+
 
 // Cutting out text for blog cards ------------------------------------------//
 
@@ -96,21 +103,77 @@ function stripHtml(html) {
 
 const showMore = document.querySelector(".showMore");
 const blogCards = document.getElementsByClassName("blogCard");
-console.log(blogCards)
+// console.log(blogCards)
 
-let counter = 4;
+let pageCounter = 1;
 
 showMore.addEventListener('click', (event) => {
 
-    counter = counter + 4;
-    console.log(counter);
-    for (let i = 0; i < blogCards.length; i++) {
-        if (blogCards[i].classList[1] === "hidden" && i < counter) {
-            blogCards[i].classList.remove("hidden");
+    
+    pageCounter++;
+    let showMoreUrl = "https://hjulbent.no/project-exam/wp-json/wp/v2/posts?per_page=4&page=" + pageCounter + "&_embed";
+
+    async function getMorePosts() {
+
+        try {
+            const response = await fetch(showMoreUrl);
+            const results = await response.json();
+            // console.log(results)
+
+            if(!Array.isArray(results)) {
+                showMore.style.display = "none";
+                return;
+            }
+            for (let i = 0; i < results.length; i++) {
+
+                let cleanText = stripHtml(results[i].excerpt.rendered)
+                let length = cleanText.length > 150 ? 150 : cleanText.length
+                let shortText = stringCutter(cleanText, length);
+
+                blogsContainer.innerHTML +=
+                    `
+                        <a href="specific.html?id=${results[i].id}" class="blogCard">
+                            <div class="blogImageWrapper">
+                                <div style="background-image: url('${results[i].featured_media_src_url}')" class="blogImage"></div>
+                            </div>
+                            <div class="blogInfo">
+                                <h3 class="blogInfoHeading">${results[i].title.rendered}</h3>
+                                 <p class="shortText">${shortText}...</p>
+                                 <div class="authorAndTime">
+                                    <p class="author">${results[i]._embedded.author[0].name}.</p>
+                                    <p class="time">${results[i].modified.slice(0, 10)}.</p>
+                                 </div>
+                            </div>
+                        </a>  
+                        `;
+            }
+
+        } catch (error) {
+            console.log(error);
         }
-        if (counter >= blogCards.length) {
-            console.log(counter);
-            showMore.style.display = "none";
-        }
+
     }
+
+    getMorePosts();
+
 });
+
+
+
+
+
+
+
+
+
+// counter = counter + 4;
+// console.log(counter);
+// for (let i = 0; i < blogCards.length; i++) {
+//     if (blogCards[i].classList[1] === "hidden" && i < counter) {
+//         blogCards[i].classList.remove("hidden");
+//     }
+//     if (counter >= blogCards.length) {
+//         console.log(counter);
+//         showMore.style.display = "none";
+//     }
+// }
